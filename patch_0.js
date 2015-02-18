78f30a80eb4febe2d8cb89addbbd7b202d8f727f
SametSisartenep <krashbuffer@gmail.com>
2015-02-18 10:47:31 +0100

    new Tree added.

--- patch_0.js
+++ patch_0.js
@@ -13,9 +13,10 @@ function Tree ( name ) {
 
 Tree.prototype.setName = function ( name ) {
   this.name = name;
-}
+};
 
 var treebol = new Tree('Treebol');
+var treebal = new Tree('Treebal');
 
 function sumUp ( n1, n2 ) {
   return n1 + n2;
