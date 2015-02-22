76105830d1fe684fd9dfe2224531f488e47abaa1
SametSisartenep <krashbuffer@gmail.com>
2015-02-22 14:49:27 +0100

    Purged files.

--- patch_0.js
+++ patch_0.js
@@ -1,11 +1,3 @@
-function informa () {
-  console.log('Vamos tos paya');
-  console.log('He finisheado.');
-  console.log('Has finisheado.');
-  console.log('Hemos finisheado.');
-  console.log('Y lo sabes...');
-}
-
 function Tree ( name ) {
   this.name = name || 'Default';
   this.branches = 2;
@@ -25,11 +17,3 @@ function sumUp ( n1, n2 ) {
 function subUp ( n1 , n2 ) {
   return n1 - n2;
 }
-
-(function ( i, callback ) {
-  for (var j = 1; j <= i; j++)
-    {
-      window.alert('Soy el número' + j);
-    }
-    Paya();
-})(10, informa);
